479098b171afa0d31a024fba3c6ea3d2df31211a
aleksi_ttu <aleksi@ttu.ee>
2019-10-06 14:40:49 +0300

    looks like caviar...

--- app/src/main/java/com/tslex/minesweeper/Game.java
+++ app/src/main/java/com/tslex/minesweeper/Game.java
@@ -3,12 +3,10 @@ package com.tslex.minesweeper;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
-import android.graphics.Point;
+import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextThemeWrapper;
-import android.view.Display;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
 import android.widget.LinearLayout;
 
@@ -17,6 +15,9 @@ public class Game extends AppCompatActivity {
     //Settings
     private static final int VERTICAL_COUNT = 15;
     private static final int HORISONTAL_COUNT = 10;
+
+    private static final int BUTTON_MARGIN = 2;
+    private static final int GAME_FIELD_MARGIN = 10;
 
     private static final int BOMBS_COUNT = 3;
 
@@ -28,6 +29,20 @@ public class Game extends AppCompatActivity {
 //        Initialize game board
         ConstraintLayout gameBoard = findViewById(R.id.gameBoard);
 
+        int orientation = this.getResources().getConfiguration().orientation;
+
+        if (orientation == 1) {
+
+            fillField(true, gameBoard);
+
+        } else {
+
+            fillField(false, gameBoard);
+        }
+    }
+
+    private void fillField(final boolean isPortraitm, ConstraintLayout gameBoard) {
+
         gameBoard.post(new Runnable() {
 
             @Override
@@ -35,21 +50,26 @@ public class Game extends AppCompatActivity {
                 ConstraintLayout gameBoard = findViewById(R.id.gameBoard);
                 LinearLayout gameField = findViewById(R.id.gameField);
 
-                int buttWidth = gameBoard.getWidth() / HORISONTAL_COUNT;
-                int buttHeight = gameBoard.getHeight() / VERTICAL_COUNT;
-                int buttSize = Math.min(buttWidth, buttHeight);
+                gameField.setOrientation((isPortraitm ? LinearLayout.VERTICAL : LinearLayout.HORIZONTAL));
+
+                int buttWidth = (gameBoard.getWidth() - GAME_FIELD_MARGIN * 2) / (isPortraitm ? HORISONTAL_COUNT : VERTICAL_COUNT);
+                int buttHeight = (gameBoard.getHeight() - GAME_FIELD_MARGIN * 2) / (isPortraitm ? VERTICAL_COUNT : HORISONTAL_COUNT);
+
+                int buttSize = Math.min(buttWidth, buttHeight) - BUTTON_MARGIN * 2;
 
                 Log.d("Main", "height: " + gameBoard.getHeight() + ", width: " + gameBoard.getWidth());
 
                 for (int y = 0; y < VERTICAL_COUNT; y++) {
 
                     LinearLayout row = new LinearLayout(gameBoard.getContext());
-                    row.setOrientation(LinearLayout.HORIZONTAL);
+                    row.setOrientation((isPortraitm ? LinearLayout.HORIZONTAL : LinearLayout.VERTICAL));
 
                     for (int x = 0; x < HORISONTAL_COUNT; x++) {
 
                         Button button = new Button(new ContextThemeWrapper(gameBoard.getContext(), R.style.FieldCell), null, 0);
-                        button.setLayoutParams(new LinearLayout.LayoutParams(buttSize, buttSize));
+                        LinearLayout.LayoutParams buttonParams = new LinearLayout.LayoutParams(buttSize, buttSize);
+                        buttonParams.setMargins(BUTTON_MARGIN, BUTTON_MARGIN, BUTTON_MARGIN, BUTTON_MARGIN);
+                        button.setLayoutParams(buttonParams);
                         row.addView(button);
                     }
 
@@ -57,6 +77,5 @@ public class Game extends AppCompatActivity {
                 }
             }
         });
-
     }
 }
